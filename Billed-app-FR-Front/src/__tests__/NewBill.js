19bac259d196a6c13e79587909caea06bd0e921c
cindon <nicolascindon@gmail.com>
2024-01-18 17:46:34 +0100

    Bill.js test ongoing

--- Billed-app-FR-Front/src/__tests__/NewBill.js
+++ Billed-app-FR-Front/src/__tests__/NewBill.js
@@ -8,26 +8,14 @@ import NewBillUI from "../views/NewBillUI.js"
 
 describe("Given I am connected as an employee", () => {
   describe("When I am on NewBill Page", () => {
-    test("Then ...", () => {
-      const html = NewBillUI()
-      document.body.innerHTML = html
-      //to-do write assertion
-      expect(screen.getAllByText('Envoyer une note de frais')).toBeTruthy()//i do this
-      expect(screen.getAllByText('Ajouter une image')).toBeTruthy()
-      expect(screen.getAllByRole('textbox')).toBeTruthy()
-      expect(screen.getAllByRole('combobox')).toBeTruthy()
-      expect(screen.getAllByRole('radio')).toBeTruthy()
-      expect(screen.getAllByRole('checkbox')).toBeTruthy()
-      expect(screen.getAllByRole('button')).toBeTruthy()
-      expect(screen.getAllByRole('img')).toBeTruthy()
-      expect(screen.getAllByRole('link')).toBeTruthy()
-      expect(screen.getAllByRole('list')).toBeTruthy()
-      expect(screen.getAllByRole('listitem')).toBeTruthy()
-      expect(screen.getAllByRole('heading')).toBeTruthy()
-      expect(screen.getAllByRole('progressbar')).toBeTruthy()
-      expect(screen.getAllByRole('separator')).toBeTruthy()
-      expect(screen.getAllByRole('status')).toBeTruthy()
-      expect(screen.getAllByRole('table')).toBeTruthy()
+    test("Then a form should be displayed", () => {
+
+      document.body.innerHTML = NewBillUI()
+
+      const form = screen.getByTestId("form-new-bill")
+      expect(form).toBeTruthy()
+
+
     })
   })
 })
